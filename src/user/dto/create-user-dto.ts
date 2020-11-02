83b5e98334f6970e572f801faeee3a1eb7ce4ff9
Yeison <toxiicsa@hotmail.com>
2020-11-02 02:17:24 -0500

    user object model

---
+++ src/user/dto/create-user-dto.ts
@@ -0,0 +1,11 @@
+export class CreateUserDto {
+    readonly firstName: string;
+    readonly secondFirstName: string;
+    readonly firstLastName: string;
+    readonly secondLastName: string;
+    readonly age: number;
+    readonly city: string;
+    readonly country: string;
+    readonly pregrade: string;
+    readonly email: string;
+}
